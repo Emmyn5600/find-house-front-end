37a5423c1a5144e998782d7a187f9fb503318e8b
emmyn5600 <emmy6618@gmail.com>
2021-08-22 23:41:32 +0200

    Implement burger button

---
+++ src/components/burger/Burger.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StyledBurger = styled.button`
+  position: absolute;
+  top: 5%;
+  left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+  
+  &:focus {
+    outline: none;
+  }
+  
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ theme }) => theme.primaryLight};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+  }
+`;
