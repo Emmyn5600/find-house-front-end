2e51dd72e2e61ef07eab16e43521cb9f58ca79f9
emmyn5600 <emmy6618@gmail.com>
2021-08-22 23:26:15 +0200

    Modify the global and theme files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../helper/global';
+import { theme } from '../helper/theme';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <h1>Hello world</h1>
+          <img src="https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg" alt="burger icon" />
+          <small>Icon made by Freepik from www.flaticon.com</small>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/helper/global.js
+++ src/helper/global.js
@@ -1,7 +1,7 @@
-import {createGlobalStyle} from 'Styled-compoents';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  html, body {
+html, body {
     margin: 0;
     padding: 0;
   }
@@ -9,13 +9,17 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
+    text-align: center;
     align-items: center;
     background: #0D0C1D;
     color: #EFFFFA;
     display: flex;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     height: 100vh;
+    width: 50%;
     justify-content: center;
     text-rendering: optimizeLegibility;
   }
-  `
+  `;
+
+export default GlobalStyles;
