773ce81851f72ea3086d5e71c0c5029ac44bb184
emmyn5600 <emmy6618@gmail.com>
2021-08-24 12:56:05 +0200

    Add the translate direction to hide the menu

--- src/components/menu/Menu.styled.js
+++ src/components/menu/Menu.styled.js
@@ -12,7 +12,7 @@ export const StyledMenu = styled.nav`
   top: 0;
   left: 0;
   transition: transform 0.3s ease-in-out;
-  
+  transform: translateX(-100%);
   @media (max-width: ${({ theme }) => theme.mobile}) {
     width: 100%;
   }
