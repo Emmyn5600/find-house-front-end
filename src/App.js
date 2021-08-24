f0b9ae5d9bcee904fd498aa9d57af0cec535ecda
emmyn5600 <emmy6618@gmail.com>
2021-08-24 13:03:21 +0200

    Install proptype for validating

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './helper/global';
 import { theme } from './helper/theme';
@@ -6,6 +6,7 @@ import { Burger, Menu } from './components';
 // import './App.css';
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -16,6 +17,10 @@ function App() {
           <h1>Hello world</h1>
           <img src="https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg" alt="burger icon" />
           <small>Made by Emmy from www.esconnect.com</small>
+        </div>
+        <div>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
         </div>
       </>
     </ThemeProvider>
