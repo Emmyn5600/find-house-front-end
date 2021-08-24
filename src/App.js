7a7596f3114407f661cf50aeabbe29ac562a4b7f
emmyn5600 <emmy6618@gmail.com>
2021-08-24 16:07:24 +0200

    Change immage with Figure tag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useRef } from 'react';
 import { ThemeProvider } from 'styled-components';
+import Figure from 'react-bootstrap/Figure';
+import FigureImage from 'react-bootstrap/FigureImage';
+import FigureCaption from 'react-bootstrap/FigureCaption';
 import { useOnClickOutside } from './hooks';
 import { GlobalStyles } from './helper/global';
 import { theme } from './helper/theme';
 import { Burger, Menu } from './components';
+
 // import './App.css';
 
 function App() {
@@ -16,11 +20,19 @@ function App() {
         <GlobalStyles />
         <Burger />
         <Menu />
-        <div className="App">
-          <h1>Find My House</h1>
-          <img src="https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg" alt="burger icon" />
-          <small>Made by Emmy</small>
-        </div>
+        <h1>Find My House</h1>
+        <Figure>
+          <FigureImage
+            width={450}
+            height={485}
+            alt="171x180"
+            src="https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg"
+          />
+          <FigureCaption>
+            You are looking for awesome house in Rwanda.
+            Keep calm you get what you are looking for.
+          </FigureCaption>
+        </Figure>
         <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
           <Menu open={open} setOpen={setOpen} />
