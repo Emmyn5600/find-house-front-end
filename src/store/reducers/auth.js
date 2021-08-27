f458c42fa204829a62cf408c4abea8e6f7f64c5e
emmyn5600 <emmy6618@gmail.com>
2021-08-27 19:31:59 +0200

    Implement the authThunk

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,3 +1,4 @@
+import { API_FETCH_START, SIGNUP_USER_SUCCESS, SIGNUP_USER_FAIL, SIGNIN_USER_SUCCESS, SIGNIN_USER_FAIL } from './actionTypes';
 
 const initialstate = {
     loading: false, 
@@ -8,14 +9,14 @@ const initialstate = {
 
 const auth = (state = initialstate, action) => {
   switch (action.type){
-      case 'API_FETCH_START':
+      case API_FETCH_START:
           return {
             ...state,
             login: true,
             error: null
           }
 
-      case 'SIGNUP_USER_SUCESSFULLY':
+      case SIGNUP_USER_SUCCESS:
           return {
               ...state, 
               isAuthenticated: true,
@@ -24,13 +25,29 @@ const auth = (state = initialstate, action) => {
               currentUser: action.payload
           }
 
-       case 'SIGNUP_USER_FAIL':
+       case SIGNUP_USER_FAIL:
            return {
                ...state,
                isAuthenticated: false,
                loading: false,
                error: action.payload
-           }   
+           }
+           
+        case SIGNUP_USER_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                currentUser: action.payload
+            }
+
+        case SIGNIN_USER_FAIL:
+            return {
+                ...state,
+               isAuthenticated: false,
+               loading: false,
+               error: action.payload
+            }
       default: 
         return state
   }
--- src/store/thunk-redux/authThunk.js
+++ src/store/thunk-redux/authThunk.js
@@ -1,3 +1,23 @@
-const signupUserAsync = () => (dispatch) => {
-    
-} 
+import axios from "axios";
+import { apiFetchStart, signUpUserSuccess, signUpUserFail, signInUserSuccess, signInUserFail } from "../actions/actionCreators";
+const endPoint = 'https://young-ravine-73545.herokuapp.com';
+
+export const signupUserAsync = (user) => async (dispatch) => {
+   dispatch(apiFetchStart())
+   try {
+       const response = await axios.post(`${endPoint}/users`, user);
+       dispatch(signUpUserSuccess(response.data));
+   } catch (error) {
+       dispatch(signUpUserFail(error));
+   }
+}
+
+export const loginUserAsync = (user) => async (dispatch) => {
+    dispatch(apiFetchStart())
+    try {
+        const response = await axios.post(`${endPoint}/auth/login`, user);
+        dispatch(signInUserSuccess(response.data));
+    } catch (error) {
+        dispatch(signInUserFail(error));
+    }
+}
