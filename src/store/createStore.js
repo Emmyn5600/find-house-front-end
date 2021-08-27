ae39aeefb59cf9290c3a1495687c25852ce13c5c
emmyn5600 <emmy6618@gmail.com>
2021-08-27 17:58:07 +0200

    Implement create store file

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,3 +1,6 @@
 import { createStore } from 'redux';
+import reducer from './reducers/index';
 
-const store  = () => createStore()
+const store  = () => createStore(reducer);
+
+export default store;
